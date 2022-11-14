22ad4b1ea21e9d0b674f5afaad5b2cf9d3ddffd5
Konstantin <konvod93@gmail.com>
2022-11-14 17:07:25 +0200

    created validation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import { registerValidation } from './validations/auth.js';
+import { validationResult } from 'express-validator';
 
 mongoose
 .connect('mongodb+srv://konvod93:Abrahadabra418@cluster0.vlgxfmv.mongodb.net/?retryWrites=true&w=majority')
@@ -11,23 +13,16 @@ const app = express();
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
 
-app.post('/auth/login', (req, res) => {
-    console.log(req.body);
-    const token = jwt.sign({
-        email: req.body.email,
-        fullName: 'Ivan Ivanov',
-    },
-    'secret123',
-    );
+app.post('/auth/register', registerValidation, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array())
+    }
     res.json({
         success: true,
-        token,
     });
-})
+});
 
 app.listen(4444, (err) => {
     if (err) {
