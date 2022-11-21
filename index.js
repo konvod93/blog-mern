d41690c667cc8b729dc01e43cf11e8fb658841eb
Konstantin <57582571+konvod93@users.noreply.github.com>
2022-11-21 20:14:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import User from './models/User.js';
 
 
 mongoose
-    .connect('mongodb+srv://konvod93:Abrahadabra418@cluster0.vlgxfmv.mongodb.net/blog?retryWrites=true&w=majority')
+    .connect('mongodb+srv://login:<password>@cluster0.vlgxfmv.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => console.log('DB Ok'))
     .catch(() => console.log('DB error', err));
 
