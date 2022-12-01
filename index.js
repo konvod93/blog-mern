16241270046790c550230d4eb5887d52597f66c9
Konstantin <konvod93@gmail.com>
2022-12-01 20:42:50 +0200

    added upload images

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import multer from 'multer'
 import mongoose from 'mongoose';
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 import checkAuth from './utils/checkAuth.js';
@@ -12,14 +13,29 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
+})
+
+const upload = multer({ storage })
+
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
-app.post('/auth/login', loginValidation, UserController.login)
-
-
+app.post('/auth/login', loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register);
-
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
+});
 
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
