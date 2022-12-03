f27f0279df6bb93bf94692f53455f1948167fae8
Konstantin <konvod93@gmail.com>
2022-12-03 15:25:29 +0200

    added handleValidationErrors

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,16 +7,10 @@ import UserModel from '../models/User.js';
 
 
 export const register = async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array());
-        }
+    try {        
 
         const password = req.body.password;
-
         const salt = await bcrypt.genSalt(10);
-
         const hash = await bcrypt.hash(password, salt)
 
         const doc = new UserModel({
