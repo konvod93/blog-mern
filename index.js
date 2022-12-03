2529c158684c7047879b990f143ced9cf0fc9f66
Konstantin <konvod93@gmail.com>
2022-12-03 16:36:13 +0200

    refactored code

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ import express from 'express';
 import multer from 'multer'
 import mongoose from 'mongoose';
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
-import checkAuth from './utils/checkAuth.js';
-import * as UserController from './controllers/UserController.js';
-import * as PostController from './controllers/PostController.js'
-import handleValidationErrors from './utils/handleValidationErrors.js';
+import { checkAuth, handleValidationErrors } from './utils/index.js';
+import { UserController, PostController } from './controllers/index.js';
+
 
 mongoose
     .connect('mongodb+srv://konvod93:Abrahadabra418@cluster0.vlgxfmv.mongodb.net/blog?retryWrites=true&w=majority')
