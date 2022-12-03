4fb82a26e7a412a5d0752c1e81a349ac9a910af1
Konstantin <57582571+konvod93@users.noreply.github.com>
2022-12-03 16:37:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { UserController, PostController } from './controllers/index.js';
 
 
 mongoose
-    .connect('mongodb+srv://konvod93:Abrahadabra418@cluster0.vlgxfmv.mongodb.net/blog?retryWrites=true&w=majority')
+    .connect('mongodb+srv://login:<password>@cluster0.vlgxfmv.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => console.log('DB Ok'))
     .catch(() => console.log('DB error', err));
 
